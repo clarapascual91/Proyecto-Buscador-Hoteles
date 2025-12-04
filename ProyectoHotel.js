2db8394bb596c6a60c76d048db5709d0cf9446c7
clarapascual91 <clpahe@alumnatflorida.es>
2025-12-04 23:17:22 +0100

    Update ProyectoHotel.js
    Actualización en buscador

--- ProyectoHotel.js
+++ ProyectoHotel.js
@@ -4,3 +4,6 @@ console.log("Hola Usuario");
 
 
 let usuarionombre = rl.question("Bienvenido, introduzca su nombre de usuario. ");
+let variablebusqueda = rl.question("Escribe la palabra clave que desees buscar: ");
+
+console.log(`Hola ${usuarionombre}, has buscado la palabra: ${variablebusqueda}`);
